fd02a1af179258e68123dc329aa05a8fdb614e3a
SH4WON AHMED <105837374+Shawon-x@users.noreply.github.com>
2024-10-16 01:04:33 +0600

    Update adduser.js

--- scripts/commands/adduser.js
+++ scripts/commands/adduser.js
@@ -13,24 +13,24 @@ module.exports.run = async function ({ api, event, args, Threads, Users }) {
 const { threadID, messageID } = event;
 const axios = require('axios')
 const link = args.join(" ")
-if(!args[0]) return api.sendMessage('Please enter the link or user ID you want to add to the group', threadID, messageID);
+if(!args[0]) return api.sendMessage('ᴘʟᴇᴀꜱᴇ ᴇɴᴛᴇʀ ᴛʜᴇ ʟɪɴᴋ ᴏʀ ᴜꜱᴇʀ ɪᴅ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴀᴅᴅ ᴛᴏ ᴛʜᴇ ɢʀᴏᴜᴘ ☑️', threadID, messageID);
 var { participantIDs, approvalMode, adminIDs } = await api.getThreadInfo(threadID);
 if(link.indexOf(".com/")!==-1) {
     const res = await axios.get(`https://golike.com.vn/func-api.php?user=${link}`);
     var uidUser = res.data.data.uid
     api.addUserToGroup(uidUser, threadID, (err) => {
-    if (participantIDs.includes(uidUser)) return api.sendMessage(`The member has to be in the group`, threadID, messageID);
+    if (participantIDs.includes(uidUser)) return api.sendMessage(`ᴛʜᴇ ᴍᴇᴍʙᴇʀ ʜᴀꜱ ᴛᴏ ʙᴇ ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ ✅`, threadID, messageID);
     if (err) return api.sendMessage(` Cannot add members to the group`, threadID, messageID);
-    else if (approvalMode && !adminIDs.some(item => item.id == api.getCurrentUserID())) return api.sendMessage(`Add successful users to the browser list`, threadID, messageID);
+    else if (approvalMode && !adminIDs.some(item => item.id == api.getCurrentUserID())) return api.sendMessage(`ᴀᴅᴅ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟ ᴜꜱᴇʀꜱ ᴛᴏ ᴛʜᴇ ʙʀᴏᴡꜱᴇʀ ʟɪꜱᴛ`, threadID, messageID);
     else return api.sendMessage(`Add members to the bar group`, threadID, messageID);
     });
     }
   else { 
     var uidUser = args[0] 
     api.addUserToGroup(uidUser, threadID, (err) => {
-    if (participantIDs.includes(uidUser)) return api.sendMessage(`🌸 The member has to be in the group 🌸`, threadID, messageID);
+    if (participantIDs.includes(uidUser)) return api.sendMessage(`🌸 ᴛʜᴇ ᴍᴇᴍʙᴇʀ ʜᴀꜱ ᴛᴏ ʙᴇ ɪɴ ᴛʜᴇ ɢʀᴏᴜᴘ 🌸`, threadID, messageID);
     if (err) return api.sendMessage(`Cannot add members to the group`, threadID, messageID);
-    else if (approvalMode && !adminIDs.some(item => item.id == api.getCurrentUserID())) return api.sendMessage(`Add successful users to the browser list`, threadID, messageID);
+    else if (approvalMode && !adminIDs.some(item => item.id == api.getCurrentUserID())) return api.sendMessage(`ᴀᴅᴅ ꜱᴜᴄᴄᴇꜱꜱꜰᴜʟ ᴜꜱᴇʀꜱ ᴛᴏ ᴛʜᴇ ʙʀᴏᴡꜱᴇʀ ʟɪꜱᴛ`, threadID, messageID);
     else return api.sendMessage(`Add members to the bar group`, threadID, messageID);
     });
   }
